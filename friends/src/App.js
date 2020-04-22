a8b4b47688121c13e68f649df54936846077604e
Vrndavana <Vrmdavama.th@gmail.com>
2020-04-21 17:33:59 -0700

    MVP

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-
 import './App.css';
-
-
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './components/Login';
+import PrivateRoute from "./components/PrivateRoute";
+import FriendList from "./components/FriendList";
+import AddFriend from './components/AddFriend';
 
 function App() {
   return (
     <div className="App">
-
-
-
+    
+      <Router>
+        <Route path='/login' component={Login}/>
+        <PrivateRoute exact path='/' component={FriendList}/>
+        {/* <Route path='/form' component={FriendForm}/> */}
+        <PrivateRoute exact path='/add-friend' component={AddFriend} />
+ 
+      </Router>
 
 
     </div>
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axiosWithAuth from"../utils/axiosWithAuth" 
+import FriendForm from "../components/FriendForm" // FriendForm is a child of FriendList
+
+export default function FriendList() {
+    const [friends, setFriends] = React.useState([])
+
+    const fetchFriends = () => {
+        axiosWithAuth()
+            .get("/api/friends")
+            .then(res => {console.log(res); setFriends(res.data)})
+            .catch(err => console.log(err.response));
+    };
+
+    React.useEffect(() => {fetchFriends();}, []);
+
+
+    return (
+        <div>
+            <h1>Add Your Own Friend!</h1>
+        {/* setFriends={setFriends} === props being connected to FriendForm from FriendList */}
+        <FriendForm setFriends={setFriends}/> 
+       
+        {friends.map(friendObj => {
+            return (<div className="enemies" key={friendObj.id}> {""} 
+                    <p>Name:{friendObj.name}</p>  {""} 
+                    <p>Age:{friendObj.age}</p>
+                    <p>Email:{friendObj.email}</p> 
+                    <button>EDIT</button>{""} 
+                    </div>)
+        })}
+        </div>
+    );
+};
